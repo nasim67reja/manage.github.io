e377ebfd9e0029a4d112489f7e62d145097ac775
nasim67reja <nasimreja67@gmail.com>
2022-03-08 21:34:05 -0800

    First Commit 🚀

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 const slide = document.querySelectorAll(".slide");
 
 slide.forEach((el, i) => {
-  el.style.transform = `translateX(${107 * i}%)`;
+  el.style.transform = `translateX(${109 * i}%)`;
 });
 
 // header
