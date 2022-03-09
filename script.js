72e50d211d132fc06cbb16ff36bcba40a07bf4f7
nasim67reja <nasimreja67@gmail.com>
2022-03-08 19:47:58 -0800

    First Commit 🚀

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ const slide = document.querySelectorAll(".slide");
 slide.forEach((el, i) => {
   el.style.transform = `translateX(${107 * i}%)`;
 });
+
+// header
+///////////////////////////////////////////////////////////////////////////////////////////////////
+const hambergur = document.querySelector(".hamburger");
+const close = document.querySelector(".close");
+const mainNav = document.querySelector(".main-nav-list");
+
+hambergur.addEventListener("click", function (e) {
+  mainNav.classList.add("nav-open");
+  hambergur.style.display = "none";
+  close.style.display = "inline-block";
+});
+
+close.addEventListener("click", function () {
+  mainNav.classList.remove("nav-open");
+  hambergur.style.display = "inline-block";
+  close.style.display = "none";
+});
